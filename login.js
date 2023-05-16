7e849eb6fc6058a93b867dc61735b7a3c2344675
Letícia <leticia.silva23@portalsesisp.org.br>
2023-05-16 14:49:22 -0300

    Logica 16052023

--- login.js
+++ login.js
@@ -1,6 +1,6 @@
 const mensagem = document.querySelector(".mensagem");
-const email = document.getElementById("email");
-const senha = document.getElementById("senha");
+const email = document.getElementById("imail");
+const senha = document.getElementById("psenha");
 const formulario = document.getElementById("formulario");
 
 formulario.onsubmit = (evento) => {
@@ -9,10 +9,21 @@ formulario.onsubmit = (evento) => {
     dados.forEach(elemento => {
         if(elemento.email === email.value && elemento.senha === senha.value){
             evento.preventDefault();
+            mensagem.innerHTML ="Logado"
+            setTimeout(()=>{
+                            window.location.assign("catalogo.html"); },2000);
             return true;
-        } else{
+        } 
+        else{
             evento.preventDefault();
             mensagem.innerHTML ="Senha ou E-mail Incorreto"
+       
         }
     });
 }
+    //localStorage.setItem("dados",JSON.stringify(dados));
+    //evento.preventDefault();
+
+    //setTimeout(()=>{
+  //      window.location.assign("catalogo.html");
+    //},2000);
--- script.js
+++ script.js
@@ -1,42 +1,53 @@
 const mensagem = document.querySelector(".mensagem");
 const formulario = document.getElementById("formulario");
-const nome = document.getElementById("nome");
-const email = document.getElementById("email");
-const senha = document.getElementById("senha");
+const nome = document.getElementById("iname");
+const email = document.getElementById("imail");
+const psenha = document.getElementById("psenha");
+const isenha = document.getElementById("isenha");
 
 formulario.onsubmit = (evento) =>{
-    if (nome.value ==""){
-        evento.preventDefault();
+    if (nome.value == ""){
         mensagem.innerHTML = "<p> Digite seu nome! </p>";
-        nome.focus();
+        evento.preventDefault();
         return null;
     }
     
     if (email.value == ""){
+        mensagem.innerHTML = "<p> Digite seu email </p>";
         evento.preventDefault();
-        mensagem.innerHTML = "<p> Digite seu email </p>"
-        email.focus();
         return null;
     }
 
-    if (senha.value == ""){
+    if (psenha.value == ""){
+        mensagem.innerHTML = "<p> Digite sua senha </p>";
         evento.preventDefault();
-        mensagem.innerHTML = "<p> Digite sua senha </p>"
-        senha.focus();
         return null;
     }
 
-    let dados = JSON.parse(localStorage.getItem("dados")) || [];dados.push({
+    if (isenha.value == ""){
+        mensagem.innerHTML = "<p> Confirme sua senha </p>";
+        evento.preventDefault();
+        return null;
+    }
+    if (psenha.value != isenha.value){
+        mensagem.innerHTML = "<p> As senhas devem ser iguais </p>";
+        evento.preventDefault();
+        return null;
+    }
+
+    let dados = JSON.parse(localStorage.getItem("dados")) || [];
+    dados.push({
         nome : nome.value,
         email : email.value,
-        senha : senha.value
+        senha : psenha.value,
+        csenha : isenha.value
     })
 
     localStorage.setItem("dados",JSON.stringify(dados));
-    evento.preventDefault();
     mensagem.innerHTML="<p>Parabéns Cadastro feito com sucesso </p>";
+    evento.preventDefault();
 
     setTimeout(()=>{
         window.location.assign("login.html");
-    },3000);
+    },2000);
 }
