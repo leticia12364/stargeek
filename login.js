c6cd628606cfc21b9b3f01db2a0fcff58dbbb471
Letícia <leticia.silva23@portalsesisp.org.br>
2023-06-05 15:08:48 -0300

    logica050623

--- login.js
+++ login.js
@@ -3,27 +3,28 @@ const email = document.getElementById("imail");
 const senha = document.getElementById("psenha");
 const formulario = document.getElementById("formulario");
 
-formulario.onsubmit = (evento) => {
+formulario.onsubmit = (evento) =>{
     let dados = JSON.parse(localStorage.getItem("dados"));
+    dados.forEach((element) => {
+        if (element.email == email.value && element.senha == senha.value) {
+            evento.preventDefault();
+            mensagem.innerHTML = "Aguarde você será redirecionado..."
 
-    dados.forEach(elemento => {
-        if(elemento.email === email.value && elemento.senha === senha.value){
-            evento.preventDefault();
-            mensagem.innerHTML ="Logado"
+            let dados = JSON.parse(sessionStorage.getItem("logado")) ||[];
+            dados.push(
+                {
+                    email:email.value
+                }
+            )
+            sessionStorage.setItem("logado",JSON.stringify(dados));
             setTimeout(()=>{
-                            window.location.assign("catalogo.html"); },2000);
+                window.location.assign("titulo.html")
+            }, 3000)
             return true;
-        } 
-        else{
+        } else{
             evento.preventDefault();
-            mensagem.innerHTML ="Senha ou E-mail Incorreto"
-       
+            mensagem.innerHTML = "Senha ou E-mail Incorreto";
         }
+       
     });
 }
-    //localStorage.setItem("dados",JSON.stringify(dados));
-    //evento.preventDefault();
-
-    //setTimeout(()=>{
-  //      window.location.assign("catalogo.html");
-    //},2000);
